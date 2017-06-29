e68b9cb8ae930d39038e309e94be2c65eae0ae0c
Mikhail Sotnikov <thehofch@gmail.com>
2017-06-29 17:13:22 +0300

    fix

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -76,16 +76,16 @@ class TelegramController extends Controller
         $from = $message['from']['first_name'];
 
         $keyboard = [
-
             [
-                'text'                  =>      'Отправить мое местоположение',
-                'request_location'      =>      true,
-            ],
-            [
-                'text'                  =>      'Не хочу',
-                'request_location'      =>      false,
-            ],
-
+                [
+                    'text'                  =>      'Отправить мое местоположение',
+                    'request_location'      =>      true,
+                ],
+                [
+                    'text'                  =>      'Не хочу',
+                    'request_location'      =>      false,
+                ],
+            ]
         ];
 
         $reply_markup = Telegram::replyKeyboardMarkup([
